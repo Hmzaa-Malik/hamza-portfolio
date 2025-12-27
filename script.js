137d13f1352c786db354b5dcfe937be757521a90
Hamza <hamzamalik3134@gmail.com>
2025-12-27 15:37:50 +0500

    Static final portfolio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*=============== SHOW / HIDE MENU (MOBILE) ===============*/
+/* ==================== MENU SHOW/HIDE ==================== */
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
@@ -15,16 +15,15 @@ if (navClose) {
   });
 }
 
-/*=============== REMOVE MENU ON LINK CLICK (MOBILE) ===============*/
-const navLinks = document.querySelectorAll(".nav-link");
+/* Remove menu on link click (mobile) */
+const navLink = document.querySelectorAll(".nav-link");
 
 function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show-menu");
 }
-navLinks.forEach((n) => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/* ==================== SCROLL SECTIONS ACTIVE LINK ==================== */
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
@@ -32,22 +31,24 @@ function scrollActive() {
 
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
+    const sectionTop = current.offsetTop - 80;
     const sectionId = current.getAttribute("id");
-    const navItem = document.querySelector(
+
+    const link = document.querySelector(
       ".nav-menu a[href*=" + sectionId + "]"
     );
+    if (!link) return;
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      navItem && navItem.classList.add("active-link");
+      link.classList.add("active-link");
     } else {
-      navItem && navItem.classList.remove("active-link");
+      link.classList.remove("active-link");
     }
   });
 }
 window.addEventListener("scroll", scrollActive);
 
-/*=============== CHANGE BACKGROUND HEADER ON SCROLL ===============*/
+/* ==================== CHANGE BACKGROUND HEADER ==================== */
 function scrollHeader() {
   const header = document.getElementById("header");
   if (this.scrollY >= 80) header.classList.add("scroll-header");
@@ -55,7 +56,7 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
-/*=============== SHOW SCROLL UP BUTTON ===============*/
+/* ==================== SHOW SCROLL UP ==================== */
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
@@ -63,59 +64,9 @@ function scrollUp() {
 }
 window.addEventListener("scroll", scrollUp);
 
-/*=============== DARK / LIGHT THEME ===============*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
-
-// Previously selected (if user chose)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
-
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
-
-if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-    iconTheme
-  );
-}
-
-themeButton.addEventListener("click", () => {
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
-});
-
-/*=============== EXPERIENCE TABS ===============*/
-const tabs = document.querySelectorAll("[data-target]");
-const tabContents = document.querySelectorAll("[data-content]");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.target);
-
-    tabContents.forEach((tc) => {
-      tc.classList.remove("experience-active");
-    });
-    target.classList.add("experience-active");
-
-    tabs.forEach((t) => {
-      t.classList.remove("experience-active");
-    });
-    tab.classList.add("experience-active");
-  });
-});
-
-/*=============== SKILLS ACCORDION ===============*/
-const skillsContent = document.getElementsByClassName("skills-content");
-const skillsHeader = document.querySelectorAll(".skills-container-header");
+/* ==================== SKILLS ACCORDION ==================== */
+const skillsContent = document.getElementsByClassName("skills-content"),
+  skillsHeader = document.querySelectorAll(".skills-container-header");
 
 function toggleSkills() {
   let itemClass = this.parentNode.className;
@@ -131,3 +82,58 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
+
+/* ==================== EXPERIENCE TABS ==================== */
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("experience-active");
+    });
+    target.classList.add("experience-active");
+
+    tabs.forEach((t) => {
+      t.classList.remove("experience-active");
+    });
+    tab.classList.add("experience-active");
+  });
+});
+
+/* ==================== DARK/LIGHT THEME ==================== */
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+// Previously selected theme (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+if (selectedTheme) {
+  if (selectedTheme === "dark") document.body.classList.add(darkTheme);
+  if (selectedIcon === "bx-moon") themeButton.classList.add(iconTheme);
+}
+
+if (themeButton) {
+  themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+  });
+}
+
+/* ==================== FOOTER YEAR ==================== */
+const yearSpan = document.getElementById("year");
+if (yearSpan) {
+  yearSpan.textContent = new Date().getFullYear();
+}
